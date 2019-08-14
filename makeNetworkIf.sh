a51a0f28df71f8696e89b99fd89f273ca3c33b77
indianatoms <ttomek.koziak@gmail.com>
2019-08-14 13:07:29 +0200

    comments added

--- makeNetworkIf.sh
+++ makeNetworkIf.sh
@@ -6,25 +6,31 @@
 OUTPUT1="$(ifconfig)"
 OUTPUT2="$(ifconfig -a)"
 
+#check wheather ifcofig is equalk to ifconfig -a
 if [ "$OUTPUT1" == "$OUTPUT2" ]
     then
+	#If they are the same that means that all NICs are configured
         echo "All network interfaces are cofigured"
     	exit 1
 fi
 
+#get all names of avaivable NICS (eth0/eht2/lo1)
 OUTPUT="$(ls /sys/class/net)"
-
+#put those names into array
 IFS=' ' read -r -a array <<< ${OUTPUT}
-
+#check all names
 for index in "${!array[@]}"
 do
+	#check if given names exist in network-scripts
 	OUTPUT2="$(ls /etc/sysconfig/network-scripts | grep ${array[index]})"
 	if [ -z "$OUTPUT2" ]
 	then
-		echo "${array[index]}"
+		#inform which intersaces are different
+		echo "${array[index]}" 
+		#reate a new config file for not configured interfaces
       		touch /etc/sysconfig/network-scripts/'ifcfg'-${array[$index]}
+		#get MAC address
 		OUTPUT="$(cat /sys/class/net/${array[$index]}/address)"
-		echo "${OUTPUT}"		
 		echo "BOOTPROTO=dhcp
 DEVICE=${array[$index]}
 HWADDR=${OUTPUT}
@@ -32,7 +38,8 @@ ONBOOT=yes
 TYPE=Ethernet
 USERCTL=no
 ZONE=public" >> /etc/sysconfig/network-scripts/'ifcfg'-${array[$index]}
-
+#put those informations into already crat4ed files.
 	fi
 done
+#restart configuration
 systemctl restart network.service
